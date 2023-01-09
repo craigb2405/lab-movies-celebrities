b7cc4e1e1b3a9130853d5d7f679a603337ba40d9
valentine974 <valentine.bathany@hotmail.fr>
2023-01-09 19:46:23 +0100

    iteration3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,11 +3,25 @@
 const Celebrity = require('../models/Celebrity.model')
 const router = require("express").Router();
 
-router.get('/create', (req, res)=>{res.render('celebrities/new-celebrity')})
-
-router.post('/celebrities/create', (req, res)=>{const {name, occupation, catchPhrase} = req.body
-Celebrity.create({name:name, occupation:occupation, catchPhrase:catchPhrase})
-res.redirect('/celebrities')
+router.get('/celebrities',(req,res)=>{  
+    Celebrity.find()
+    .then((result)=>{
+        console.log(result)
+        res.render('celebrities/celebrity-list',{ result})
+    })
+    .catch(err=>console.log('the error', err))
 })
+
+router.get('/new-celebrity', (req, res)=>{res.render('celebrities/new-celebrity')})
+
+router.post('/new-celebrity', (req, res)=>{
+    console.log(req.body)
+    const {name, occupation, catchPhrase} = req.body 
+    Celebrity.create({name:name, occupation:occupation, catchPhrase:catchPhrase})
+    res.redirect('/celebrities') 
+})
+
+
+
 
 module.exports = router;
