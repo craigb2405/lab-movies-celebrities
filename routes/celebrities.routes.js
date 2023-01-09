61e35934bfb11ff988d6cd056096e0d906367154
valentine974 <valentine.bathany@hotmail.fr>
2023-01-09 20:29:08 +0100

    solved route

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -11,15 +11,16 @@ router.post('/new-celebrity', (req, res)=>{
     console.log(req.body)
     const {name, occupation, catchPhrase} = req.body 
     Celebrity.create({name:name, occupation:occupation, catchPhrase:catchPhrase})
-    res.redirect('celebrities/celebrity-list') 
+    res.redirect('/celebrities/celebrities') 
 })
 
-router.get('/celebrity-list',(req,res)=>{  
+router.get('/celebrities',(req,res)=>{  
     Celebrity.find()
     .then((result)=>{
         console.log(result)
-        res.render('celebrities/celebrity-list',{result})
+        res.render('celebrities/celebrities',{ result})
     })
+    .catch(err=>console.log('the error', err))
 })
 
 
