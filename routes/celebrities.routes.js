127bcb4e879052f3dfb75dee5b38277f523f390b
Craig Butler <craig.butler@jrbsolutions.co.uk>
2023-01-09 19:06:14 +0000

    iteration 3 almost complete

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,14 +3,7 @@
 const Celebrity = require('../models/Celebrity.model')
 const router = require("express").Router();
 
-router.get('/celebrities',(req,res)=>{  
-    Celebrity.find()
-    .then((result)=>{
-        console.log(result)
-        res.render('celebrities/celebrity-list',{ result})
-    })
-    .catch(err=>console.log('the error', err))
-})
+
 
 router.get('/new-celebrity', (req, res)=>{res.render('celebrities/new-celebrity')})
 
@@ -18,10 +11,16 @@ router.post('/new-celebrity', (req, res)=>{
     console.log(req.body)
     const {name, occupation, catchPhrase} = req.body 
     Celebrity.create({name:name, occupation:occupation, catchPhrase:catchPhrase})
-    res.redirect('/celebrities') 
+    res.redirect('celebrities/celebrity-list') 
 })
 
-
+router.get('/celebrity-list',(req,res)=>{  
+    Celebrity.find()
+    .then((result)=>{
+        console.log(result)
+        res.render('celebrities/celebrity-list',{result})
+    })
+})
 
 
 module.exports = router;
