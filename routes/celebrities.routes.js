2117e7b9b11acdc35b3e41a3549337764175abfd
Craig Butler <craig.butler@jrbsolutions.co.uk>
2023-01-10 18:32:37 +0000

    updated to match Omars code demo

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,28 +1,38 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 
-const Celebrity = require('../models/Celebrity.model')
+const Celebrity = require("../models/Celebrity.model");
 const router = require("express").Router();
 
 //Create
 
-router.get('/celebrities/create', (req, res)=>{res.render('celebrities/new-celebrity')})
+router.get("/celebrities/create", (req, res) => {
+  res.render("celebrities/new-celebrity");
+});
 
-router.post('/celebrities/create', (req, res)=>{
-    console.log(req.body)
-    const {name, occupation, catchPhrase} = req.body 
-    Celebrity.create({name:name, occupation:occupation, catchPhrase:catchPhrase})
-    res.redirect('/celebrities') 
-})
-
-router.get('/celebrities/',(req,res)=>{  
-    Celebrity.find()
-    .then((result)=>{
-        res.render('celebrities/celebrities',{ result})
+router.post("/celebrities/create", (req, res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.create({
+    name: name,
+    occupation: occupation,
+    catchPhrase: catchPhrase,
+  })
+    .then((result) => {
+      res.redirect("/celebrities");
     })
-    .catch((error)=>{console.log(error)
-       
-    })
-})
+    .catch((error) => {
+      console.log(error);
+      res.redirect("/new-celebrity");
+    });
+});
 
+router.get("/celebrities/", (req, res) => {
+  Celebrity.find()
+    .then((result) => {
+      res.render("celebrities/celebrities", { result });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 module.exports = router;
